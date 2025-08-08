9dede6c7e57bd1453dafb522dfe45aaec20e861b
Andreas Backström <mozzo242@gmail.com>
2025-08-08 18:54:14 +0200

    Set full github pages url as base

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,43 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: true,
+	base: "https://mozzo1000.github.io/munchlist/",
+	plugins: [
+		react(),
+		tailwindcss(),
+		VitePWA({
+			registerType: "prompt",
+			injectRegister: true,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
 
-    manifest: {
-      name: 'Munchlist',
-      short_name: 'Munchlist',
-      description: 'Simple grocery list app',
-      theme_color: '#059669',
-      display: 'standalone',
-    },
+			manifest: {
+				name: "Munchlist",
+				short_name: "Munchlist",
+				description: "Simple grocery list app",
+				theme_color: "#059669",
+				display: "standalone",
+			},
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+			},
 
-    devOptions: {
-      enabled: true,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+			devOptions: {
+				enabled: true,
+				navigateFallback: "index.html",
+				suppressWarnings: true,
+				type: "module",
+			},
+		}),
+	],
+});
