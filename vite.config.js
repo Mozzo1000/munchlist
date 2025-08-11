464f136cb19841f58cdfc1e684e8379bb9a6e43a
Andreas Backström <mozzo242@gmail.com>
2025-08-11 20:42:02 +0200

    Add build options to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "https://munchlist.app",
+	build: {
+		sourcemap: false,
+		target: "esnext",
+		minify: "esbuild",
+		terserOptions: {
+			compress: {
+				drop_console: true,
+			},
+		},
+	},
 	plugins: [
 		react(),
 		tailwindcss(),
